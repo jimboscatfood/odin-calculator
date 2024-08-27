66bc4d8c2f3e510a42535d54f63e369e01fece4d
jimboscatfood <149596389+jimboscatfood@users.noreply.github.com>
2024-08-27 21:37:33 +0800

    Create event listener for operators and equal buttons
    Select all operators button and append event listener to all the
    operators buttons to listen for click.
    
    Once any operator button is clicked, the operator variable will be
    assigned a value accoridngly and the value in display will be temporarily stored in the prevNum
    variable.
    
    The event listener for equal button will then call the operate function

--- script.js
+++ script.js
@@ -22,29 +22,77 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
     switch(operator) {
         case '+':
-            add();
+            results = add(num1, num2);
+            break;
         case '-':
-            subtract();
+            results = subtract(num1, num2);
+            break;
         case '*':
-            multiply();
+            results = multiply(num1, num2);
+            break;
         case '/':
-            divide();
+            results = divide(num1, num2);
+            break;
     }
+    display.textContent = `${results}`;
 }
+
+//Store and initialise the display value for next step
+let results = 0;
+let prevNum;
+let operator;
+let presentNum;
+
 
 //Populate the display when you click the number buttons
 const display = document.querySelector(".display");
 const numKeys = document.querySelectorAll(".numKey");
+const operators = document.querySelectorAll(".operator");
+display.textContent = `${results}`;
 
+//Listen for number keys
 numKeys.forEach((button) => {
-    button.addEventListener("click", () => {
+    button.addEventListener("click", function() {
         const keyInput = button.textContent;
-        display.textContent += `${keyInput}`;
-        console.log(keyInput);
+        if (display.textContent === '0') {
+            display.textContent = `${keyInput}`;
+        }
+        else if (display.textContent === `${prevNum}`) {
+            display.textContent = `${keyInput}`;
+        }
+        else {
+            display.textContent += `${keyInput}`;
+        }
+        presentNum = display.textContent;
     });
 });
 
-//Store the display value for next step
-let firstNum;
-let operator;
-let secondNum;
+//Listen for operator
+operators.forEach((button) => {
+    button.addEventListener("click", () => {
+        //Only lock in the value of the firstNum when an operator is clicked
+        prevNum = display.textContent;
+        //Change value of operator based on button clicked
+        switch (button.id) {
+            case ("divide"):
+                operator = '/';
+                break;
+            case ("multiply"):
+                operator = '*';
+                break;
+            case ("subtract"):
+                operator = '-';
+                break;
+            case ("add"):
+                operator = '+';
+                break;
+        }
+    });
+});
+
+//Listen for Equal button
+const equal = document.querySelector("#equal");
+equal.addEventListener("click", () => {
+    operate(operator, prevNum, presentNum);
+});
+
